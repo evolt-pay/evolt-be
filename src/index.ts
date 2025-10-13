9ce3ceaf7f4018144ea0b30e5dbb96fa25389c2a
Sadiq Mustapha Aji <sadiqajimustapha@gmail.com>
2025-10-13 23:35:12 +0100

    fixed volt escrow

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ app.register(corePlugin);
 const start = async (): Promise<void> => {
     try {
         await app.ready();
-        await app.listen({ port: config.app.port });
+        await app.listen({ host: "0.0.0.0", port: config.app.port });
     } catch (err) {
         app.log.error(err);
         process.exit(1);

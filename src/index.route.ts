6f2bd7b20633f6ede27dc061bdce7f118b9d0591
Sadiq Mustapha Aji <sadiqajimustapha@gmail.com>
2025-10-18 20:00:04 +0100

    investor portfolio api

--- src/index.route.ts
+++ src/index.route.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import authRoutes from "./auth/auth.route.js";
-import airdropRoutes from "airdrop/airdrop.route.js";
+import airdropRoutes from "./airdrop/airdrop.route.js";
 import userRoutes from "./user/user.route.js";
 import investorRoutes from "./investor/investor.route.js";
 import businessRoutes from "./business/business.route.js";
@@ -8,7 +8,7 @@ import invoiceRoutes from "./invoice/invoice.route.js";
 import investmentRoutes from "./investment/investment.route.js";
 import corporateRoutes from "./corporate/corporate.route.js";
 import poolRoutes from "./pool/pool.route.js";
-import swapRoutes from "swap/swap.route.js";
+import swapRoutes from "./swap/swap.route.js";
 
 export const indexRoute = async (app: FastifyInstance) => {
     app.register(authRoutes, { prefix: "/api/v1/auth" });
